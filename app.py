69c5df510b03537624d937b21cf3cea23475947c
Anand Ramaka <ramakaanand@gmail.com>
2023-07-03 19:46:57 -0700

    Updated API resources

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ print(driver)
 
 # https://www.biostars.org/p/271718/
 
+def getNeighbors( node_name ):
+    with driver.session() as session:
+        result = session.run("MATCH (sourceNode {Name: $name})-[:INTERACTS_WITH]->(neighborNode) RETURN neighborNode", name=node_name)
+        nodes = [record['neighborNode']['Name'] for record in result]
+        return nodes
+
 class NodeResource(Resource):
     def get(self):
         with driver.session() as session:
@@ -17,18 +23,34 @@ class NodeResource(Resource):
             nodes = [record['n']['Name'] for record in result]
             return {'nodes': nodes}, 200
 
+class Neighbors(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('Name', required=True)
         args = parser.parse_args()
         node_name = args['Name']
 
-        with driver.session() as session:
-            result = session.run("MATCH (sourceNode {Name: $name})-[:INTERACTS_WITH]->(neighborNode) RETURN neighborNode", name=node_name)
-            nodes = [record['neighborNode']['Name'] for record in result]
-            return {'neighbors': nodes}, 201
+        neighbors = getNeighbors(node_name)
+        return {'neighbors': neighbors}, 201
+
+class ValidMedication(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('Names', required=True)
+        args = parser.parse_args()
+        names = args['Names']
+
+        neighbors = { getNeighbors(n) for n in names }
+
+        intersection = set(names) & neighbors
+
+        return {'valid': not intersection, 'intersection' : intersection}, 201
+
+
 
 api.add_resource(NodeResource, '/nodes')
+api.add_resource(Neighbors, '/neighbors')
+api.add_resource(Neighbors, '/valid')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- test.py
+++ test.py
@@ -12,6 +12,18 @@ url = 'http://127.0.0.1:8000/nodes'  # Replace with your API URL
 #     print('Error:', response.status_code)
 
 
+data = {'names': ["DB00382", "DB00762"]}
+
+response = requests.post(url, json=data)
+
+if response.status_code == 201:
+    neighbors = response.json()['neighbors']
+    print('Neighbors:', neighbors)
+else:
+    print('Error:', response.text)
+
+
+
 data = {'Name': "DB00382"}
 
 response = requests.post(url, json=data)
